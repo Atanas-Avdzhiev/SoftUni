d17406885c696e6587228844980610b701b365ef
atanas9610 <165182913+atanas9610@users.noreply.github.com>
2024-10-29 21:32:09 +0200

    JS Applications Tasks

--- JS Advanced/Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
+++ JS Advanced/Applications/Asynchronous Programming - Exercise/05. Accordion/accordion.js
@@ -1,3 +1,42 @@
-function solution() {
-    //TODO .....
+async function solution() {
+    const url = 'http://localhost:3030/jsonstore/advanced/articles/list';
+    const response = await fetch(url);
+    const data = await response.json();
+    const dataArray = Object.entries(data);
+    const main = document.querySelector('#main');
+
+    dataArray.forEach(async (item) => {
+        const divAccordion = document.createElement('div');
+        divAccordion.setAttribute('class', 'accordion');
+
+        const url2 = `http://localhost:3030/jsonstore/advanced/articles/details/${item[1]._id}`;
+
+        const response2 = await fetch(url2);
+        const data2 = await response2.json();
+
+        divAccordion.innerHTML = `<div class="head">
+                <span>${item[1].title}</span>
+                <button class="button" id="${item[1]._id}">More</button>
+            </div>
+            <div class="extra">
+                <p>${data2.content}</p>
+            </div>`;
+
+        const currentButton = divAccordion.querySelector('button');
+
+        currentButton.addEventListener('click', () => {
+            const divToShow = divAccordion.querySelector('.extra');
+            if (currentButton.textContent === 'More') {
+                divToShow.style.display = 'block';
+                currentButton.textContent = 'Less';
+            }
+            else if (currentButton.textContent === 'Less') {
+                divToShow.style.display = 'none';
+                currentButton.textContent = 'More';
+            }
+        });
+
+        main.appendChild(divAccordion);
+    })
 }
+window.addEventListener('load', solution);
--- JS Advanced/Applications/Asynchronous Programming - Exercise/06.Blog/app.js
+++ JS Advanced/Applications/Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,5 +1,46 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    const [postBtn, posts, viewBtn, comments] = ['#btnLoadPosts', '#posts', "#btnViewPost", '#post-comments']
+        .map(sel => document.querySelector(sel))
 
+    postBtn.addEventListener('click', onPost)
+    viewBtn.addEventListener('click', onView)
+    let postbody = '';
+    async function onPost(e) {
+
+        const response = await fetch(`http://localhost:3030/jsonstore/blog/posts`);
+        const data = await response.json();
+        posts.innerHTML = "";
+        Object.entries(data).forEach(([key, value]) => {
+            let optionElement = document.createElement('option');
+            optionElement.value = key
+            optionElement.textContent = value.title
+            posts.appendChild(optionElement)
+            postbody = value.body
+        })
+    }
+
+    async function onView(e) {
+
+        let select = [...posts.children].find((x) => x.selected == true)
+        const [res, comment] = await Promise.all([
+            fetch(`http://localhost:3030/jsonstore/blog/posts/${select.value}`),
+            fetch(`http://localhost:3030/jsonstore/blog/comments`)
+        ]);
+        const data = await res.json();
+        comments.innerHTML = "";
+        let postTitle = posts.querySelector("option:checked");
+        document.getElementById('post-title').textContent = postTitle.textContent;
+        document.getElementById('post-body').textContent = postbody
+        const dataComments = await comment.json();
+
+        Object.values(dataComments).forEach((el) => {
+            if (data.id == el.postId) {
+                let li = document.createElement('li')
+                li.textContent = el.text
+                li.id = el.id
+                comments.appendChild(li);
+            }
+        });
+    }
+}
 attachEvents();
