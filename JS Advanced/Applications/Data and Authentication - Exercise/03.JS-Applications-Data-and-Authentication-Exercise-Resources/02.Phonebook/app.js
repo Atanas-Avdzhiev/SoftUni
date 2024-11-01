76afe4f1b08dd4dbe33da5989a1e8538a78db2c4
atanas9610 <165182913+atanas9610@users.noreply.github.com>
2024-11-01 23:23:47 +0200

    JS Applications Tasks

--- JS Advanced/Applications/Data and Authentication - Exercise/03.JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
+++ JS Advanced/Applications/Data and Authentication - Exercise/03.JS-Applications-Data-and-Authentication-Exercise-Resources/02.Phonebook/app.js
@@ -1,5 +1,63 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    const loadButton = document.querySelector('#btnLoad');
+    const createButton = document.querySelector('#btnCreate');
+    const ul = document.querySelector('#phonebook');
 
+    const baseURL = 'http://localhost:3030/jsonstore/phonebook';
+
+    loadButton.addEventListener('click', () => {
+        fetch(baseURL)
+            .then(res => res.json())
+            .then(data => {
+                ul.innerHTML = '';
+                const dataValues = Object.values(data);
+                dataValues.forEach(person => {
+                    const li = document.createElement('li');
+                    li.textContent = `${person.person}: ${person.phone}`;
+
+                    const deleteButton = document.createElement('button');
+                    deleteButton.textContent = 'Delete';
+
+                    deleteButton.addEventListener('click', () => {
+                        const deleteURL = `http://localhost:3030/jsonstore/phonebook/${person._id}`;
+                        fetch(deleteURL, {
+                            method: 'DELETE'
+                        })
+                        // .then(res => res.json())
+                        // .then(data => {
+                        //     //loadButton.click();
+                        // })
+                        // .catch();
+                    })
+                    li.appendChild(deleteButton);
+                    ul.appendChild(li);
+                })
+            })
+        //.catch();
+    })
+
+    createButton.addEventListener('click', () => {
+
+        const personInput = document.querySelector('#person');
+        const phoneInput = document.querySelector('#phone');
+
+        fetch(baseURL, {
+            method: 'POST',
+            body: JSON.stringify({
+                "person": personInput.value,
+                "phone": phoneInput.value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                personInput.value = '';
+                phoneInput.value = '';
+                loadButton.click();
+            })
+        //.catch();
+    })
+}
 attachEvents();
--- JS Advanced/Applications/Data and Authentication - Exercise/03.JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
+++ JS Advanced/Applications/Data and Authentication - Exercise/03.JS-Applications-Data-and-Authentication-Exercise-Resources/03.Students/app.js
@@ -1,1 +1,66 @@
-console.log('TODO...');
+const baseURL = 'http://localhost:3030/jsonstore/collections/students';
+const form = document.querySelector('body div div form');
+
+fetch(baseURL)
+    .then(res => res.json())
+    .then(data => {
+        const dataValues = Object.values(data);
+
+        const tbody = document.querySelector('tbody');
+
+        dataValues.forEach(student => {
+            const tr = document.createElement('tr');
+            const tdFirstName = document.createElement('td');
+            tdFirstName.textContent = student.firstName;
+
+            const tdLastName = document.createElement('td');
+            tdLastName.textContent = student.lastName;
+
+            const tdFacultyNumber = document.createElement('td');
+            tdFacultyNumber.textContent = student.facultyNumber;
+
+            const tdGrade = document.createElement('td');
+            tdGrade.textContent = student.grade;
+
+            tr.appendChild(tdFirstName);
+            tr.appendChild(tdLastName);
+            tr.appendChild(tdFacultyNumber);
+            tr.appendChild(tdGrade);
+            tbody.appendChild(tr);
+        })
+    })
+    .catch(err => alert(err.message));
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const firstNameInput = document.querySelector('[name=firstName]');
+    const lastNameInput = document.querySelector('[name=lastName]');
+    const facultyNumberInput = document.querySelector('[name=facultyNumber]');
+    const gradeInput = document.querySelector('[name=grade]');
+
+    if (typeof firstNameInput.value !== 'string' || firstNameInput.value === '' ||
+        typeof lastNameInput.value !== 'string' || lastNameInput.value === '' ||
+        typeof facultyNumberInput.value !== 'string' || (!/^[0-9]+$/.test(facultyNumberInput.value)) ||
+        gradeInput.value === '' || (!/^\d+(\.\d+)?$/.test(gradeInput.value))) {
+        return;
+    }
+
+    fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName: firstNameInput.value,
+            lastName: lastNameInput.value,
+            facultyNumber: facultyNumberInput.value,
+            grade: gradeInput.value
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            location.reload();
+        })
+        .catch(err => alert(err.message));
+})
